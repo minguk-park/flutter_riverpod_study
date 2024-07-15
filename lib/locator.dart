d6e97cc79061be832a7da0b0c9edfbfd84b65a9e
minguk-park <qkralsrnr206@naver.com>
2024-07-15 13:21:50 +0900

    [feat] 불필요한 import 삭제

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_riverpod_study/datasources/local_datasource.dart';
-import 'package:flutter_riverpod_study/datasources/post_datasource.dart';
 import 'package:flutter_riverpod_study/datasources/remote_datasource.dart';
-import 'package:flutter_riverpod_study/repositories/post_repository.dart';
 import 'package:get_it/get_it.dart';
 
 final GetIt locator = GetIt.I;
@@ -10,6 +8,7 @@ void setupLocator() {
   locator.registerLazySingleton(() => RemoteDataSource());
   locator.registerLazySingleton(() => LocalDataSource());
 
+  // 이렇게도 가능
   // locator.registerLazySingleton<RemoteDataSource>(() => RemoteDataSource());
   // locator.registerLazySingleton<LocalDataSource>(() => LocalDataSource());
 }
