c5870955e58934fc8444715331c41ed49193556d
minguk-park <qkralsrnr206@naver.com>
2024-08-21 14:17:39 +0900

    [feat] 주석 수정

--- lib/providers/provider_type_provider.dart
+++ lib/providers/provider_type_provider.dart
@@ -1,18 +1,22 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // Provider
+// 주로 변경되지 않는 데이터를 제공할 때 사용
+// - 이미 계산된 값
+// - 단순한 읽기 전용 상태
 final messageProvider = Provider<String>((ref) {
   return "Hello, Riverpod!";
+});
+
+// StateProvider
+// 상태를 변경해야 하는 경우
+final counterProvider = StateProvider<int>((ref) {
+  return 0; // 초기 상태 값
 });
 
 // FutureProvider
 
 // StreamProvider
-
-// StateProvider
-final counterProvider = StateProvider<int>((ref) {
-  return 0; // 초기 상태 값
-});
 
 // NotifierProvider
 
